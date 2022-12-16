f29b7c1c5bc9ed6c83e4c5bbe57e4047dbc861fd
IsmayilAbbasli69 <ismayil.abbasli333@gmail.com>
2022-12-17 00:50:13 +0400

    progress2

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("INDEX");
+  res.status(200).render("index",{
+page_name:"index"
+
+  });
 });
+app.get("/about", (req, res) => {
+    res.status(200).render("about",{
+        page_name:"about"
+    });
+  });
+  app.get("/contact", (req, res) => {
+    res.status(200).render("contact",{
+        page_name:"contact"
+    });
+  });
+
 
 app.listen(port, () => {
   console.log(`${port} is active`);
